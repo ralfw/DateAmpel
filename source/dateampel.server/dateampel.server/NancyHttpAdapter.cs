ee930bd677d11f41c2dc4809f9f818ff0585d9d3
Ralf Westphal <info@ralfw.de>
2013-01-30 13:47:23 +0100

    Wackelndes Skelett mit HTML Seiten

--- source/dateampel.server/dateampel.server/NancyHttpAdapter.cs
+++ source/dateampel.server/dateampel.server/NancyHttpAdapter.cs
@@ -10,7 +10,34 @@ namespace dateampel.server
     {
         public NancyHttpAdapter()
         {
-            Get["/"] = _ => "ASP.NET - Hello, World! " + DateTime.Now;
+            Get["/"] = _ => View["ampel_eckdaten_erfassen"];
+            Post["/ampel_anlegen"] = _ =>
+            {
+                var viewModel = new
+                {
+                    Titel = Request.Form.DateName + " trifft " + Request.Form.InitiatorName,
+                    InitiatorEmail = Request.Form.InitiatorEmail,
+                    DateEmail = Request.Form.DateEmail
+                };
+                return View["ampel_quittung", viewModel];
+            };
+
+            Get["/ampel/{partnerId}"] = _ =>
+            {
+                var viewModel = new
+                {
+                    AmpelId = "???"
+                };
+                return View["ampel_dashboard", viewModel];
+            };
+            Post["/date_bewerten"] = _ =>
+            {
+                var viewModel = new
+                {
+                    AmpelId = Request.Form.AmpelId
+                };
+                return View["ampel_dashboard", viewModel];
+            };
         }
     }
 }
