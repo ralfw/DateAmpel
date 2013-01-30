655903bc09aaafc1913b824a68f3994e48060afc
Ralf Westphal <info@ralfw.de>
2013-01-30 12:31:39 +0100

    Wackelndes Skelett für Nancy

---
+++ source/dateampel.nancy.server/dateampel.nancy.server/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+using Nancy.Hosting.Self;
+
+namespace dateampel.nancy.server
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var host = new NancyHost(new Uri("http://localhost:8000"));
+            host.Start(); // start hosting
+
+            Console.Write("Nancy running... Press any key to exit");
+            Console.ReadKey();
+            host.Stop(); // stop hosting
+        }
+    }
+
+    public class HttpPortal : NancyModule
+    {
+        public HttpPortal()
+        {
+            Get["/"] = _ => "Hello, World! " + DateTime.Now;
+        }
+    }
+}
