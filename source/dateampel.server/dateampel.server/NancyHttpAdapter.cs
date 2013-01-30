0a90f7f69b14ba28b62d994f5d807dccd8bef704
Ralf Westphal <info@ralfw.de>
2013-01-30 12:54:06 +0100

    Umstieg auf ASP.NET Hosting für Nancy

---
+++ source/dateampel.server/dateampel.server/NancyHttpAdapter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace dateampel.server
+{
+    public class NancyHttpAdapter : NancyModule
+    {
+        public NancyHttpAdapter()
+        {
+            Get["/"] = _ => "ASP.NET - Hello, World! " + DateTime.Now;
+        }
+    }
+}
